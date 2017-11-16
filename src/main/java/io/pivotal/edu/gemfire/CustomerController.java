f4890655e77b930bb3aefd7cffe30e686eabad07
Mark Secrist <msecrist@pivotal.io>
2017-11-16 10:05:07 -0700

    Fixed missing PathVariable reference and renamed the method to get a customer by ID

--- src/main/java/io/pivotal/edu/gemfire/CustomerController.java
+++ src/main/java/io/pivotal/edu/gemfire/CustomerController.java
@@ -1,7 +1,11 @@
 package io.pivotal.edu.gemfire;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,15 +13,22 @@ public class CustomerController {
 	
 	@Autowired
 	private CustomerService service;
-	
+		
 	@RequestMapping("/customers")
 	public String customers() {		
 		return service.getAllCustomers().toString();
 	}
 	
 	@RequestMapping("/customer/{customerId}")
-	public String  addCustomers(Integer customerId) {
+	public String  getCustomer(@PathVariable Integer customerId) {
+		System.out.println("customerId: " + customerId);
 		return service.getCustomerById(customerId).toString();
+	}
+	
+	@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Requested resource not found")
+	@ExceptionHandler(RuntimeException.class)
+	public void handleException(RuntimeException exception) {
+		System.out.println("Caught exception: "  + exception);
 	}
 
 }
